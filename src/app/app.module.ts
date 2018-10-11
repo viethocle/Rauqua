194164f0bd274c97bb75563024a6a7a671bc197d
Linh Tran <hlinh273@gmail.com>
2018-10-11 14:18:21 +0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './router/app-routing.module';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderLayoutComponent } from './components/layout/header-layout/header-layout.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HomeComponent,
+    HeaderLayoutComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
