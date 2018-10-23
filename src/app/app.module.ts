ffc234c416938c6020d87e041da7c5b60e9109e0
Linh Tran <hlinh273@gmail.com>
2018-10-23 11:06:43 +0700

    [i18n] add languages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -25,7 +25,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'en'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
